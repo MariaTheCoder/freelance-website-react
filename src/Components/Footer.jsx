c28dd9fe4e93b6653b87a9f4e98b666b35eac835
Maria T. Andersen <mteilmann93@gmail.com>
2023-11-20 10:29:37 +0100

    Import icons and incorporate them into the footer element

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,36 @@
 import style from "./Footer.module.css";
+import PhoneIcon from "../assets/Icons/icons8-phone-48.png";
+import EmailIcon from "../assets/Icons/icons8-email-48.png";
+import XingIcon from "../assets/Icons/icons8-xing-48.png";
+import LinkedInIcon from "../assets/Icons/icons8-linkedin-48.png";
+import YouTubeIcon from "../assets/Icons/icons8-youtube-48.png";
 
 function Footer() {
-  return <footer className={style.Footer}>Footer</footer>;
+  return (
+    <footer className={style.Footer}>
+      <div className="contact-info">Kerim Güney</div>
+      <div className="contact-info">
+        <img className="icons" src={PhoneIcon} alt="Phone icon" />
+        Give me a call
+      </div>
+      <div className="contact-info">
+        <img className="icons" src={EmailIcon} alt="Email icon" />
+        Send me an e-mail
+      </div>
+      <div className="contact-info">
+        <img className="icons" src={XingIcon} alt="Xing icon" />
+        {"Let's connect on Xing"}
+      </div>{" "}
+      <div className="contact-info">
+        <img className="icons" src={LinkedInIcon} alt="LinkedIn icon" />
+        {"Let's connect on LinkedIn"}
+      </div>
+      <div className="contact-info">
+        <img className="icons" src={YouTubeIcon} alt="YouTube icon" />
+        Subscribe to my YouTube channel
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
