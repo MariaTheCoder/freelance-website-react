969f6e0f896518ccda24fc466e1334094af131bd
Maria T. Andersen <mteilmann93@gmail.com>
2023-11-20 09:17:54 +0100

    Add a Projects component and give it a very simple style for now

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import NavigationBar from "./Components/NavigationBar";
 import Profile from "./Components/Profile";
 import Skills from "./Components/Skills";
+import Projects from "./Components/Projects";
 import "./App.css";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <NavigationBar />
       <Profile />
       <Skills />
+      <Projects />
     </header>
   );
 }
---
+++ src/Components/Projects.jsx
@@ -0,0 +1,7 @@
+import style from "./Projects.module.css";
+
+function Projects() {
+  return <div className={style.Projects}>Projects</div>;
+}
+
+export default Projects;
