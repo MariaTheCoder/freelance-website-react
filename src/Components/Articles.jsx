59c32da6bce203323811535347b2d35f6f697940
Maria T. Andersen <mteilmann93@gmail.com>
2023-11-20 11:38:43 +0100

    Use a git container for the articles

--- src/Components/Articles.jsx
+++ src/Components/Articles.jsx
@@ -1,7 +1,21 @@
 import style from "./Articles.module.css";
 
 function Articles() {
-  return <div className={style.Articles}>Articles</div>;
+  return (
+    <div className={style.Articles}>
+      <h2>Read my latest articles</h2>
+      <div className={style["grid-container"]}>
+        <p className={style["date"]}>2023-12-12</p>
+        <p className={style["header"]}>Some title</p>
+        <p className={style["date"]}>2023-11-05</p>
+        <p className={style["header"]}>Some title</p>
+        <p className={style["date"]}>2023-10-23</p>
+        <p className={style["header"]}>Some title</p>
+        <p className={style["date"]}>2023-08-16</p>
+        <p className={style["header"]}>Some title</p>
+      </div>
+    </div>
+  );
 }
 
 export default Articles;
