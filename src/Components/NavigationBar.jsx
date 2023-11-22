6580312651f36a00f86a10ee3c375dde6d7b62b0
Maria T. Andersen <mteilmann93@gmail.com>
2023-11-22 08:56:48 +0100

    Replace logo on the homepage

--- src/Components/NavigationBar.jsx
+++ src/Components/NavigationBar.jsx
@@ -1,10 +1,11 @@
+import logo from "../assets/Logo/icons8-k-100.png";
 import style from "./NavigationBar.module.css";
 
 function NavigationBar() {
   return (
     <div className={style.NavigationBar}>
       <a className={style["navigation-link"]} href=".">
-        Logo
+        <img src={logo} alt="Logo" />
       </a>
       <a className={style["navigation-link"]} href="http://">
         {"Let's work together"}
