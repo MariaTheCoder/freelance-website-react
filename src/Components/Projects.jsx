c29a89db66d9f4336b2d9329bd4f58222882b492
Maria T. Andersen <mteilmann93@gmail.com>
2023-11-22 11:45:15 +0100

    Get ready to re-style the projects component by tossing out previous styles and content

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,22 +1,16 @@
-import projectImage1 from "../assets/StockPhotos/pexels-brett-sayles-1597776.jpg";
-import projectImage2 from "../assets/StockPhotos/pexels-dan-cristian-pădureț-1476321.jpg";
-import projectImage3 from "../assets/StockPhotos/pexels-jeshootscom-442587.jpg";
-import projectImage4 from "../assets/StockPhotos/pexels-johannes-plenio-1105379.jpg";
-import projectImage5 from "../assets/StockPhotos/pexels-luis-gomes-546819.jpg";
-import projectImage6 from "../assets/StockPhotos/pexels-pixabay-4158.jpg";
 import style from "./Projects.module.css";
 
 function Projects() {
   return (
     <div className={style.Projects}>
-      <h2>My Projects</h2>
+      <h2>Projects</h2>
       <a
         className="project"
         href="https://www.w3schools.com"
         target="_blank"
         rel="noreferrer"
       >
-        <img src={projectImage1} alt="Project image 1" />
+        <p>1</p>
       </a>
       <a
         className="project"
@@ -24,7 +18,7 @@ function Projects() {
         target="_blank"
         rel="noreferrer"
       >
-        <img src={projectImage2} alt="Project image 2" />
+        <p>2</p>
       </a>
       <a
         className="project"
@@ -32,7 +26,7 @@ function Projects() {
         target="_blank"
         rel="noreferrer"
       >
-        <img src={projectImage3} alt="Project image 3" />
+        <p>3</p>
       </a>
       <a
         className="project"
@@ -40,23 +34,7 @@ function Projects() {
         target="_blank"
         rel="noreferrer"
       >
-        <img src={projectImage4} alt="Project image 4" />
-      </a>
-      <a
-        className="project"
-        href="https://www.w3schools.com"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <img src={projectImage5} alt="Project image 5" />
-      </a>
-      <a
-        className="project"
-        href="https://www.w3schools.com"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <img src={projectImage6} alt="Project image 6" />
+        <p>4</p>
       </a>
     </div>
   );
