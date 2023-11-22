f63ca70979ce4722a948117de823a21b0dfa6b68
Maria T. Andersen <mteilmann93@gmail.com>
2023-11-22 10:01:54 +0100

    Replace article headers with longer ones to control display of longer article headers

--- src/App.jsx
+++ src/App.jsx
@@ -11,22 +11,23 @@ function App() {
   const [articles, setArticles] = useState([
     {
       date: "2023-12-10",
-      title: "Some title",
+      title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
       link: "https://en.wikipedia.org/wiki/React_(software)",
     },
     {
       date: "2023-10-02",
-      title: "Some title",
+      title: "Sed tincidunt eget risus in finibus",
       link: "https://en.wikipedia.org/wiki/IBM_i",
     },
     {
       date: "2023-08-20",
-      title: "Some title",
+      title: "Vestibulum viverra magna elit, a elementum velit viverra at",
       link: "https://en.wikipedia.org/wiki/World_Wide_Web",
     },
     {
       date: "2023-07-31",
-      title: "Some title",
+      title:
+        "Ut placerat, arcu faucibus blandit fringilla, leo libero feugiat mauris, vitae pulvinar nunc ligula eget neque",
       link: "https://en.wikipedia.org/wiki/Computer",
     },
   ]);
